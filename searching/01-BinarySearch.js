c451b16d327a6f30381b4b9e82094c797c0f146d
hunmix <youarewang@126.com>
2019-12-13 17:14:31 +0800

    fix binarySearch

--- searching/01-BinarySearch.js
+++ searching/01-BinarySearch.js
@@ -3,22 +3,21 @@ function binarySearch (arr, target) {
   // 在arr[l...r]中寻找target
   let l = 0
   let r = arr.length - 1
-  // 防止大整数溢出
-  let mid = l + (r - l) / 2
+  let mid = l + Math.floor((r - l) / 2)
   while (l <= r) {
     if (arr[mid] === target) {
       return mid
     }
-    if (arr[mid] < target) {
-      // arr[mid+1...r]
+    // [mid+1...r]
+    if (target > arr[mid]) {
       l = mid + 1
+    // [l...mid-1]
     } else {
-      // arr[l...mid-1]
       r = mid - 1
     }
-    mid = l + (r - l) / 2
+    mid = l + Math.floor((r - l) / 2)
   }
-  return -1
+  return - 1
 }
-console.log(binarySearch([1,2,3,4,5,6,7], 9))
+console.log(binarySearch([-1,0,3,5,9,12], 9))
 module.exports = binarySearch
