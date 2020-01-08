559e662a5906afa7e595c6d07dab14c99abc06d8
hunmix <youarewang@126.com>
2020-01-09 00:17:40 +0800

    u

--- README.md
+++ README.md
@@ -43,6 +43,7 @@
 - [ ] 145-BinaryTreePostorderTraversal 迭代算法
 - [x] 279-PerfectSquares 尝试转换成图
 - [x] 127-WordLadder 双向bfs优化
+- [ ] 126-WordLadderII (不想做了)!!!放放
 - 动态规划
 - [x] 343-IntegerBreak 优化: 贪心
 - [x] 198-HouseRobber 不熟
---
+++ algrothm/stack/126-WordLadderII.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} beginWord
+ * @param {string} endWord
+ * @param {string[]} wordList
+ * @return {string[][]}
+ */
+var findLadders = function(beginWord, endWord, wordList) {
+};
+
+console.log(findLadders('hit', 'cog', ['hot','dot','dog','lot','log','cog']))
+console.log(findLadders('hit', 'cog', ['hot','dot','dog','lot','log']))
+console.log(findLadders('hot', 'dog', ['hot','dog']))
